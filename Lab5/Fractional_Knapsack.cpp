9326a693262d998ee6286fdea36216f9a6c460dc
Yeasir Arafat <yeasirar@gmail.com>
2021-12-06 13:03:21 +0600

    Add

--- Lab5/Fractional_Knapsack.cpp
+++ Lab5/Fractional_Knapsack.cpp
@@ -6,17 +6,21 @@ struct Item_List{
     float v,w;
 };
 bool compare_to(Item_List c1, Item_List c2){
-    return (c2.v/c2.w)<(c1.v/c1.w);
+    return (c1.v/c1.w)>(c2.v/c2.w);
 }
 float Fractional_Knapsack(int W, Item_List item[], int size){
     sort(item, item+size,compare_to);
-    int w = W;
-    int sum = 0;
+    float w = W;
+    float sum = 0.0;
     for(int i=0; w>0 && i<size; i++){
         float wi = item[i].w;
+        float vi = item[i].v;
         w=w-wi;
         if(w>=0)
-            sum+=item[i].v;
+            sum+=vi;
+        else if(w<0){
+            sum+=(wi+w)*(vi/wi);
+        }
     }
     return sum;
 }
@@ -24,5 +28,5 @@ float Fractional_Knapsack(int W, Item_List item[], int size){
 int main(){
     Item_List A[] ={{ 60, 10 }, { 100, 20 }, { 120, 30 }};
     int size=sizeof(A)/sizeof(A[0]);
-    cout<< "Max Value: "<<Fractional_Knapsack(49,A,size);
+    cout<< "Max Value: "<<Fractional_Knapsack(50,A,size);
 }
