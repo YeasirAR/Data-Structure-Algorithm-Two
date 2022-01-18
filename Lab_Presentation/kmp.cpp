4ea7f4a0a4a8ad92f26eeff68ba0e68a8cd89d5c
Yeasir Arafat <yeasirar@gmail.com>
2022-01-18 08:04:14 +0600

    Add

---
+++ Lab_Presentation/kmp.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> calc_ps(string pattern,int p_len) {
+    vector<int> lps(p_len);
+    lps[0]=0;
+    int length = 0;
+    int i = 1;
+    while (i < p_len) {
+        if (pattern[i] == pattern[length]){
+            lps[i] = length + 1;
+            length++; i++;
+        }
+        else {
+            if (length != 0)
+                length = lps[length - 1];
+            else{
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    return lps;
+}
+
+bool kmp (string str, string pattern,
+            int p_len,int s_len,vector<int>& foundAt) {
+
+    vector<int> lps = calc_ps(pattern,p_len);
+
+    int i = 0, j = 0;
+    bool flag=false;
+    while (i < s_len) {
+        if (str[i] == pattern[j]){
+            i++; j++;
+        }
+        else {
+            if (j != 0) 
+                j = lps[j - 1];
+            else 
+                i++;
+        }
+
+        if (j == p_len) {
+            foundAt.push_back(i-p_len);
+            j = lps[j - 1];
+            flag = true;
+        }
+    }
+    return flag;
+}
+
+int main() {
+    string str="ACBACACBAACBACAACBAC";
+    string pattern="ACBAC";
+    int p_len = pattern.length();
+    int s_len = str.length();
+    vector<int> foundAt;
+    bool isFound = kmp(str, pattern,p_len,s_len,foundAt);
+    if(isFound){
+        for(int i:foundAt){
+            cout<<"Found at index "<<i<<" to "<<i+p_len-1<<endl;
+        }
+    }
+    else{
+        cout<<"Not Found";
+    }
+
+    return 0;
+}
